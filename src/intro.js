822bb3c36797b1e5f534c668149094e408b50616
Sabaris <sabaris.elango@gmail.com>
2024-06-07 13:36:56 +0530

    Progress opening page

--- src/intro.js
+++ src/intro.js
@@ -1,15 +1,23 @@
-import { Ship, GameBoard, Player } from "./battleship";
+import { player1, player2 } from "./battleship";
 
 function opening() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
 
+  const introPage = document.createElement("div");
+  introPage.classList.add("introPage");
+
   const welcome = document.createElement("div");
+  welcome.classList.add("welcome");
   welcome.textContent = "Welcome to battleship game";
 
   const welcome2 = document.createElement("div");
+  welcome2.classList.add("welcome2");
   welcome2.textContent = "Place your Carrier";
 
   const rotate = document.createElement("button");
+  rotate.classList.add("rotate");
   rotate.textContent = "Rotate";
+
+  player1.gamePlacement.forEach((plots) => {});
 }
