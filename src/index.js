bb5fc9bc364dd435ad3058358f259bddeb7a9eb6
Sabaris <sabaris.elango@gmail.com>
2024-06-12 13:59:35 +0530

    Setup home page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "./style.css";
-import { player1, player2 } from "./battleship";
-
+import { player1, player1Board, player2, player2Board } from "./battleship";
+import opening from "./intro";
 function setup() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
+  opening();
 }
+setup();
