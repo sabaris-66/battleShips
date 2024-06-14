4a59e5215a745b9d0cdfc9fa3115a25c121ba499
Sabaris <sabaris.elango@gmail.com>
2024-06-15 03:49:30 +0530

    Add computer.js to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./style.css";
 import { player1, player1Board, player2, player2Board } from "./battleship";
 import opening from "./intro";
+import computerPlacement from "./computer";
 function setup() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
   opening();
+  computerPlacement();
 }
 setup();
