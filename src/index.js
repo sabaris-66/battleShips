a30620c40f5cacbd7b6eb8147bc544264f9568f8
Sabaris <sabaris.elango@gmail.com>
2024-06-15 14:01:37 +0530

    Import battlePage.js to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import "./style.css";
 import { player1, player1Board, player2, player2Board } from "./battleship";
 import opening from "./intro";
 import computerPlacement from "./computer";
+import battlePage from "./battlePage";
 function setup() {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
   opening();
   computerPlacement();
+
+  const play = document.querySelector(".playButton");
+  play.addEventListener("click", () => {
+    battlePage();
+  });
 }
 setup();
