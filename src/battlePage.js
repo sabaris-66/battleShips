c3e388b524124bcce5ff542a9cc90a85c09c15a9
Sabaris <sabaris.elango@gmail.com>
2024-06-17 02:27:45 +0530

    Use result function to diaplay result

--- src/battlePage.js
+++ src/battlePage.js
@@ -1,4 +1,7 @@
-function battlePage() {
+import ending from "./result";
+import setup from ".";
+
+function battlePage(player1, player1Board, player2, player2Board) {
   const body = document.querySelector("body");
   const content = document.querySelector(".content");
 
@@ -22,8 +25,6 @@ function battlePage() {
   let player2Ground = document.createElement("div");
   player2Ground.classList.add("player2Ground");
   playerGround.append(player2Ground);
-
-  let playerGridNo = 0;
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       let player1Div = document.createElement("div");
@@ -33,6 +34,135 @@ function battlePage() {
       let player2Div = document.createElement("div");
       player2Div.classList.add("player2Plots");
       player2Ground.append(player2Div);
+
+      let attackedShip;
+      let removedShip;
+
+      if (player1.gamePlacement[i][j] >= 0 && player1.gamePlacement[i][j] < 5) {
+        player1Div.classList.add("colorIntroShips");
+      }
+
+      player2Div.addEventListener("click", () => {
+        if (
+          player2.gamePlacement[i][j] != "attacked" &&
+          player2.gamePlacement[i][j] != "missed"
+        ) {
+          if (
+            player2.gamePlacement[i][j] >= 0 &&
+            player2.gamePlacement[i][j] < 5
+          ) {
+            attackedShip = player2.gamePlacement[i][j];
+            console.log(attackedShip);
+            player2.playerShips[attackedShip].hits++;
+            player2.gamePlacement[i][j] = "attacked";
+            player2Div.classList.add("colorAttackedShips");
+
+            console.log(player2.gamePlacement);
+            // console.log(player2.playerShips);
+            // console.log(player2.playerShips[attackedShip].placed);
+            if (
+              player2.playerShips[attackedShip].hits ==
+              player2.playerShips[attackedShip].length
+            ) {
+              player2.playerShips[attackedShip].sunk = true;
+              console.log(attackedShip);
+              console.log(player2.playerShips);
+              let p2ShipsSunk = player2.playerShips.every((ship) => ship.sunk);
+              console.log(p2ShipsSunk);
+              if (p2ShipsSunk == true) {
+                console.log("p1 won");
+                finalBattle.remove();
+                ending(
+                  "Player 1 Won!!!",
+                  player1,
+                  player1Board,
+                  player2,
+                  player2Board
+                );
+                // const resultPage = document.querySelector(".resultPage");
+                // const replay = document.querySelector(".replay");
+                // replay.addEventListener("click", () => {
+                //   resultPage.remove();
+                //   setup(player1, player1Board, player2, player2Board);
+                // });
+                // print p1 won
+              }
+            }
+          } else if (player2.gamePlacement[i][j] == "O") {
+            player2Div.classList.add("colorWrongShips");
+            player2.gamePlacement[i][j] = "missed";
+          }
+          let randRow = Math.floor(Math.random() * 10);
+          let randCol = Math.floor(Math.random() * 10);
+          while (
+            player1.gamePlacement[randRow][randCol] == "attacked" ||
+            player1.gamePlacement[randRow][randCol] == "missed"
+          ) {
+            randRow = Math.floor(Math.random() * 10);
+            randCol = Math.floor(Math.random() * 10);
+          }
+          if (
+            player1.gamePlacement[randRow][randCol] >= 0 &&
+            player1.gamePlacement[randRow][randCol] < 5
+          ) {
+            attackedShip = player1.gamePlacement[randRow][randCol];
+            player1.playerShips[attackedShip].hits++;
+            player1.gamePlacement[randRow][randCol] = "attacked";
+            // player1Div.classList.remove("colorIntroShips");
+            color();
+
+            // console.log(player2.playerShips);
+            // console.log(player2.playerShips[attackedShip].placed);
+            // player1Div.classList.add("colorAttackedShips");
+            if (
+              player1.playerShips[attackedShip].hits ==
+              player1.playerShips[attackedShip].length
+            ) {
+              console.log(player1.playerShips);
+              let p1ShipsSunk = player2.playerShips.every((ship) => ship.sunk);
+              console.log(p1ShipsSunk);
+              if (p1ShipsSunk == true) {
+                console.log("p2 won");
+                finalBattle.remove();
+                // print p2 won
+                ending(
+                  "Player 2 Won!!!",
+                  player1,
+                  player1Board,
+                  player2,
+                  player2Board
+                );
+                // const resultPage = document.querySelector(".resultPage");
+                // const replay = document.querySelector(".replay");
+                // replay.addEventListener("click", () => {
+                //   resultPage.remove();
+                //   setup(player1, player1Board, player2, player2Board);
+                // });
+              }
+            }
+          } else {
+            player1.gamePlacement[randRow][randCol] = "missed";
+            // console.log(player2.playerShips);
+            // console.log(player2.playerShips[attackedShip].placed);
+            color();
+            // player1Div.classList.add("colorWrongShips");
+          }
+        }
+      });
+    }
+  }
+  function color() {
+    const player1grid = document.querySelectorAll(".player1Plots");
+    let current = 0;
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (player1.gamePlacement[i][j] == "attacked") {
+          player1grid[current].classList.add("colorAttackedShips");
+        } else if (player1.gamePlacement[i][j] == "missed") {
+          player1grid[current].classList.add("colorWrongShips");
+        }
+        current++;
+      }
     }
   }
 }
